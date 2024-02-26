83d573e82170e329824990a1f34974ddffcee16d
Brian Takita <brian.takita@gmail.com>
2024-02-25 22:49:09 -0500

    ./layout: site__header__hyop:
    	site__header__hamburger__hyop→site__header__menu_handle__hyop
    	hamburger_→menu_handle_
    	hamburger__set→menu_handle__set
    	is_active_: ∋ menu_handle_: .toggled→.active

--- layout/site__header__hyop.ts
+++ layout/site__header__hyop.ts
@@ -12,10 +12,10 @@ export function site__header__navlinks_login__hyop(navlinks_login:HTMLElement) {
 export function site__header__navlinks_menu__hyop(navlinks_menu:HTMLElement) {
 	navlinks_menu__set(browser_ctx__ensure(), navlinks_menu)
 }
-export function site__header__hamburger__hyop(hamburger:HTMLButtonElement) {
+export function site__header__menu_handle__hyop(menu_handle:HTMLButtonElement) {
 	const ctx = browser_ctx__ensure()
-	hamburger__set(ctx, hamburger)
-	hamburger.addEventListener('click', ()=>{
+	menu_handle__set(ctx, menu_handle)
+	menu_handle.addEventListener('click', ()=>{
 		is_active__toggle(ctx)
 	})
 }
@@ -49,18 +49,19 @@ function navlinks_login__open(ctx:wide_ctx_T) {
 	navlinks_menu_(ctx)!.classList.add('hidden')
 	navlinks_login_(ctx)!.classList.remove('hidden')
 }
-const [, hamburger_, hamburger__set] = be_sig_triple_<HTMLElement|undefined>(()=>undefined)
+const [, menu_handle_, menu_handle__set] = be_sig_triple_<HTMLElement|undefined>(
+	()=>undefined)
 const [, nav_layer_, nav_layer__set] = be_sig_triple_<HTMLElement|undefined>(()=>undefined)
 const [, is_active_, is_active__set] = be_sig_triple_(
 	()=>false
 ).add((ctx, is_active$)=>memo_(()=>{
 	if (is_active$()) {
 		navlinks_(ctx)!.classList.add('!visible', '!scale-100', '!opacity-100', '!lg:translate-y-0')
-		hamburger_(ctx)!.classList.add('toggled')
+		menu_handle_(ctx)!.classList.add('active')
 		nav_layer_(ctx)!.classList.add('origin-top', 'scale-y-100')
 	} else {
 		navlinks_(ctx)!.classList.remove('!visible', '!scale-100', '!opacity-100', '!lg:translate-y-0')
-		hamburger_(ctx)!.classList.remove('toggled')
+		menu_handle_(ctx)!.classList.remove('active')
 		nav_layer_(ctx)!.classList.remove('origin-top', 'scale-y-100')
 	}
 }))
